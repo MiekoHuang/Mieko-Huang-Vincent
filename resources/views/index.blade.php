86a011e6bef10c4b062e40a026466285b6ff3537
MiekoHuang <cvin2826@gmail.com>
2023-03-22 12:32:38 +0700

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('Layout/index')
+@section('title', 'Vincent | CV')
+@section('content')
+
+    <div>
+        <div class="d-flex align-items-center mt-5 py-3">
+            <div class="d-flex container">
+                <div class="d-flex w-100 gap-md-5 align-items-center align-items-md-start flex-column flex-sm-row">
+                    <div>
+                        <div class="mt-5">
+                            <h1 class="fw-bold">Welcome to my website</h1>
+                            <div class="mt-5">
+                                <h2>Mieko Huang Vincent</h2>
+                                <h4>Student</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
